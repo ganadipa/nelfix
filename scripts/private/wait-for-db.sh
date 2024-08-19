2c7110230ebcbd7850895b3ac87864e8a8389d51
ganadipa <nyomanganadipa@gmail.com>
2024-08-19 21:08:50 +0700

    Fix deployment issue

--- scripts/private/wait-for-db.sh
+++ scripts/private/wait-for-db.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-until nc -z -v -w30 $(echo $DATABASE_URL | sed -E 's/.*\/\/([^:]+):[^@]+@([^:]+):([0-9]+).*/\2 \3/') 2>/dev/null
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+# Extract the host and port from the DATABASE_URL
+host_port=$(echo $DATABASE_URL | sed -E 's/.*\/\/([^:]+):[^@]+@([^:]+):([0-9]+).*/\2 \3/')
+
+# Wait until Postgres is ready
+until nc -z -v -w30 $host_port 2>/dev/null
 do
   echo "Waiting for Postgres to be ready..."
   sleep 2
